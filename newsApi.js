86b36724b553aa925c49d36103dd7d44ac5fcc59
VarunBhandia <varunbhandia@gmail.com>
2019-06-08 12:36:50 +0530

    initial commit

---
+++ newsApi.js
@@ -0,0 +1,5 @@
+const apiKey = '582a48190f7245de87d9683943b14fc5';
+const topHeadlinesUrl =
+  'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + apiKey;
+
+export { apiKey, topHeadlinesUrl };
